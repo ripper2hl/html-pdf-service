b8a3f1d2362ab0a8a29a72f24685c681477fb42b
Jesús Israel Perales Martínez <jesus.perales@famsa.com>
2018-08-25 15:45:05 -0500

    file

--- routes/html.js
+++ routes/html.js
@@ -2,25 +2,32 @@ var express = require('express');
 var router = express.Router();
 var pdf = require('html-pdf');
 var iconv  = require('iconv-lite');
+var multer = require('multer');
+var upload = multer({ dest: '/tmp/' });
+var fs = require('fs');
 
-/* GET users listing. */
-router.get('/', function(req,
-   res, next) {
+router.get('/', function(req,res, next) {
   crearPdf(req.query.html, function (file){
     res.sendFile(file);
   });
 });
 
-function crearPdf(html , callback){
-  html = iconv.decode(new Buffer(html), "utf-8");
-   pdf.create( html ).toFile(function(err, file) {
-     if (err) {
-       console.log(err);
-       callback(err);
-     }else{
-       console.log(file.filename);
-       callback(file.filename);
-     }
-   });
+router.post('/', upload.single('html') , function(req,res, next) {
+  crearPdf(req.file, function (file){
+    res.sendFile(file);
+  });
+});
+
+function crearPdf( fileHtml , callback){
+  fs.readFile( fileHtml.path, {encoding: 'utf-8'}, function (err , data){   
+    pdf.create( data ).toFile(function(err, file) {
+      if (err) {
+        console.log(err);
+        callback(err);
+      }else{
+        callback(file.filename);
+      }
+    });
+  });
 }
 module.exports = router;
